e684c3b447cafbbbb5b01aabed0e92f5d48619f7
VoltageViper99 <voltagepcrepairs@protonmail.com>
2026-07-18 03:22:43 +1000

    Rename package to librarysentinel

---
+++ librarysentinel/constants.py
@@ -0,0 +1,23 @@
+# constants.py
+
+AUDIO_TYPES = {
+    ".flac",
+    ".ape",
+    ".wv",
+    ".wav",
+    ".alac",
+    ".m4a",
+    ".mp3",
+    ".ogg",
+    ".opus",
+    ".aac",
+    ".dsf",
+    ".dff",
+}
+
+IMAGE_TYPES = {
+    ".jpg",
+    ".jpeg",
+    ".png",
+    ".webp",
+}
---
+++ release_notes.md
@@ -0,0 +1,12 @@
+gh release create v0.2.0 \
+  --title "v0.2.0 - Cue Hunter" \
+  --notes "## Added
+
+- Cue sheet detection
+- Empty album detection
+- Audio format statistics
+
+## Changed
+
+- Refactored scanner into cleaner functions
+- Improved filesystem scanning
